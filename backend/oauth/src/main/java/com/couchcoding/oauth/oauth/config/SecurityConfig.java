597d8f469fb1e82cb500f7820991460e5b9e0f04
godongs <asd236678@gmail.com>
2021-09-10 18:31:48 +0900

    add firebase key

--- backend/oauth/src/main/java/com/couchcoding/oauth/oauth/config/SecurityConfig.java
+++ backend/oauth/src/main/java/com/couchcoding/oauth/oauth/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.couchcoding.oauth.oauth.filter.JwtFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated().and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling()
