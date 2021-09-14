77b3e393b73c3d7cc05463531dc7a4ad5855d63d
godongs <asd236678@gmail.com>
2021-09-14 15:25:33 +0900

    Refactor folders

--- backend/oauth/src/main/java/com/couchcoding/oauth/oauth/config/FirebaseConfig.java
+++ backend/oauth/src/main/java/com/couchcoding/oauth/oauth/config/FirebaseConfig.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Configuration
 public class FirebaseConfig {
     @Bean
--- backend/oauth/src/main/java/com/couchcoding/oauth/oauth/controller/RegisterInfo.java
+++
@@ -1,8 +0,0 @@
-package com.couchcoding.oauth.oauth.controller;
-
-import lombok.Data;
-
-@Data
-public class RegisterInfo {
-    private String nickname;
-}
